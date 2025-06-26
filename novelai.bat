a97cfb085ae89b94226179b4fbe3c2af8ea3b9ce
prome02 <34807295+prome02@users.noreply.github.com>
2025-06-26 14:59:32 +0800

    ..

--- novelai.bat
+++ novelai.bat
@@ -1,11 +1,11 @@
 @echo off
 REM By default it is configured for LM Studio running locally. Uncomment and set API key to use Chat-GPT or other services. More at https://platform.openai.com/docs/api-reference/chat/create
-REM SET OPENAI_API_BASE=https://api.openai.com/v1
+SET OPENAI_API_BASE=https://api.openai.com/v1
 REM SET OPENAI_API_KEY=xxxxxxxxxxxxxxx
-REM SET OPENAI_API_MODEL=gpt-4o
-REM SET RETRY_COUNT=0
-REM SET SERVER_PORT=8124
-REM SET NOVELAI_DEBUG=True
+SET OPENAI_API_MODEL=gpt-4o
+SET RETRY_COUNT=0
+SET SERVER_PORT=8124
+SET NOVELAI_DEBUG=False
 
 REM Uncomment the following line to generate longer novel outlines
 REM SET NOVELAI_TEMPLATES_DIR=templates/alt,templates
@@ -15,7 +15,7 @@ REM
 REM Check if a parameter was passed to the batch file
 if "%1"=="" (
     REM No parameter was provided. Start server and open browser.
-    python main.py start --inbrowser
+    python main.py start --inbrowser --share
 ) else (
     REM Parameter provided: %1
     REM Execute commands based on the input parameter
